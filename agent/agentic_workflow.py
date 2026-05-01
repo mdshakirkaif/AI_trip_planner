255c3bc69d33e5b45ecb10ed71b47f86264c34d7
mdshakirkaif <mdshakirkaif07@gmail.com>
2026-05-01 23:46:29 +0530

    folder update

---
+++ agent/agentic_workflow.py
@@ -0,0 +1,16 @@
+
+
+
+
+class GraphBuilder():
+    def __init__(self):
+        pass
+    
+    def agent_function(self):
+        pass
+    
+    def build_graph(self):
+        pass
+    
+    def __call__(self):
+        pass
