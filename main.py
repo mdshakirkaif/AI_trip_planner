7d3fa4933a279f23fa364ccfc388c2bb63bada71
mdshakirkaif <mdshakirkaif07@gmail.com>
2026-05-12 18:39:18 +0530

    frontend and backend done

--- main.py
+++ main.py
@@ -1,6 +1,37 @@
-def main():
-    print("Hello from ai-trip-planner!")
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from pydantic import BaseModel
+from agent.agentic_workflow import GraphBuilder
 
+app=FastAPI()
 
-if __name__ == "__main__":
-    main()
+class QuearyRequest(BaseModel):
+    question:str
+
+@app.post('/'queary)
+async def queary_travel_agent(queary:QuearyRequest):
+    try:
+        print(queary)
+        graph =GraphBuilder(model_provider='groq')
+        react_app=graph()
+        
+        png_graph = react_app.get_graph().draw_mermaid_png()
+        with open("my_graph.png", "wb") as f:
+            f.write(png_graph)
+
+        print(f"Graph saved as 'my_graph.png' in {os.getcwd()}")
+        # Assuming request is a pydantic object like: {"question": "your text"}
+        messages={"messages":[queary.question]}
+        
+        output=react_app.invoke(messages)
+        
+        # If result is dict with messages:
+        if isinstance(output, dict) and "messages" in output:
+            final_output = output["messages"][-1].content  # Last AI response
+        else:
+            final_output = str(output)
+        
+        return {"answer": final_output}
+    except Exception as e:
+        return JSONResponse(status_code=500, content={"error": str(e)})
+        
