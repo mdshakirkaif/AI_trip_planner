34802745befd6d1e933741ae56b8f6b5df1496e4
mdshakirkaif <mdshakirkaif07@gmail.com>
2026-05-13 11:41:01 +0530

    weather tool done

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from agent.agentic_workflow import GraphBuilder
+from utils.save_to_document import save_document
+from starlette.responses import JSONResponse
+import os
+import datetime
 from dotenv import load_dotenv
 from pydantic import BaseModel
-from agent.agentic_workflow import GraphBuilder
+load_dotenv()
 
 app=FastAPI()
 
 class QuearyRequest(BaseModel):
     question:str
 
-@app.post('/'queary)
+@app.post('/queary')
 async def queary_travel_agent(queary:QuearyRequest):
     try:
         print(queary)
